e666555c8e88089d4342a801c6d37e891aa64953
Ujjwal5200 <savagerockstar29@gmail.com>
2025-10-20 07:25:48 -0700

    project structure with all dependency

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="ujjwal kaushik",
+    author_email="ujjwal7202@gmail.com",
+    packages=find_packages()
+)
